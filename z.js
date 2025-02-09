910c56e2fb97532664d7d036cc53c5be2750bb11
xx-qq <sshyta@mail.ru>
2025-02-09 11:47:32 +0300

    commit

--- z.js
+++ z.js
@@ -1,1 +1,1 @@
-let input = prompt(); if (input.length > 2 ) {console.log(input.slice(-2))} else if (input.length < 2) {console.log ("Введенная строка слишком короткая")}
+let input = prompt(); if (input.length > 2 ) {console.log(input.slice(-2))} else if (input.length < 2) {console.log ("Введенная строка слишком короткая")} 
