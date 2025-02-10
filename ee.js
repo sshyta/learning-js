b3804e2dc95339c7dbac3918f7a201ea726ef95f
xx-qq <sshyta@mail.ru>
2025-02-10 12:48:31 +0300

    Commit

---
+++ ee.js
@@ -0,0 +1,1 @@
+const numElements = Number(prompt("Введите количество элементов в массиве:")); const userArray = []; const lengths = []; for (let i = 0; i < numElements; i++) { userArray[i] = prompt() }; for (const word of userArray) { lengths.push(word.length) }; console.log(lengths);
